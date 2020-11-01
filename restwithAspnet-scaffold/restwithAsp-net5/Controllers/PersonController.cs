59d5248fed0275c8a14291b3bc852bee655662b6
farley-melo <farleynirvana@gmail.com>
2020-11-01 17:40:10 -0300

    Classe Person implementada

---
+++ restwithAspnet-scaffold/restwithAsp-net5/Controllers/PersonController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace restwithAsp_net5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber,string secondNumber)
+        {
+            return BadRequest("Invalid Input");
+        }
+
+      
+
+     
+      
+
+      
+
+       
+    }
+}
